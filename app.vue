cd356a55239971c17a6af89c05aab64d7cd3b759
David Nahodyl <david@gearboxgo.com>
2024-04-25 13:05:23 -0400

    moved app config

--- app.vue
+++ app.vue
@@ -1,3 +1,14 @@
+<script setup lang="ts">
+useHead({
+  title: "Auth Tester",
+  htmlAttrs: {
+    class: "dark",
+  },
+  bodyAttrs: {
+    class: "bg-gray-700 text-gray-200",
+  },
+});
+</script>
 <template>
   <div>
     <NuxtLayout>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,15 +1,6 @@
 <script setup lang="ts">
 import NavBar from "~/components/nav/NavBar.vue";
 import ContentWidthWrapper from "~/components/ContentWidthWrapper.vue";
-
-useHead({
-  htmlAttrs: {
-    class: "dark",
-  },
-  bodyAttrs: {
-    class: "bg-gray-700 text-gray-200",
-  },
-});
 </script>
 
 <template>

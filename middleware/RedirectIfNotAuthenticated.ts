99d0d9327b524d7bc2734e68964c88a848219944
David Nahodyl <david@gearboxgo.com>
2024-05-20 16:04:39 -0400

    consistent return, comments

--- middleware/RedirectIfNotAuthenticated.ts
+++ middleware/RedirectIfNotAuthenticated.ts
@@ -1,9 +1,11 @@
-export default defineNuxtRouteMiddleware((to, from) => {
-  // redirect the user to the login screen if they're not authenticated
-
+export default defineNuxtRouteMiddleware(() => {
+  // check if the user is logged in
   const { loggedIn } = useUserSession();
 
+  // redirect the user to the login screen if they're not authenticated
   if (!loggedIn.value) {
     return navigateTo("/login");
   }
+
+  return null;
 });

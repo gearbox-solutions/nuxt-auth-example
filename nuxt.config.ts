b8b50693228a0ba5c674ded1f6c2c38e773a96a5
David Nahodyl <david@gearboxgo.com>
2024-05-20 12:07:21 -0400

    parseint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,7 @@ export default defineNuxtConfig({
   runtimeConfig: {
     database: {
       host: process.env.DB_HOST ?? "127.0.0.1", // can be set with DB_HOST environment variable at build time or overwritten at runtime with NUXT_DB_HOST
-      port: process.env.DB_PORT ?? 3306,
+      port: parseInt(process.env.DB_PORT ?? "3306", 10),
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,

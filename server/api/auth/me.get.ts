480eaf26a594f9a6611eb82ee70762034b9e2458
David Nahodyl <david@gearboxgo.com>
2024-05-20 16:31:37 -0400

    require that the user is logged in

--- server/api/auth/me.get.ts
+++ server/api/auth/me.get.ts
@@ -1,6 +1,9 @@
-export default defineEventHandler(async (event) => {
-  const user = await auth.user(event);
+import requireUserLoggedIn from "~/server/utils/requireUserLoggedIn";
 
+export default defineEventHandler(async (event) => {
+  await requireUserLoggedIn(event);
+
+  const user = await auth.user(event);
   return {
     id: user.id,
     name: user.name,

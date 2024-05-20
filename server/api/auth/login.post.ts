f58e7a3d03fc2e1a822bb8f989d7eeb4109bbc29
David Nahodyl <david@gearboxgo.com>
2024-05-20 15:44:38 -0400

    moved to auth helper

--- server/api/auth/login.post.ts
+++ server/api/auth/login.post.ts
@@ -1,32 +1,7 @@
-import users from "~/database/schema/users";
-import getDatabase from "~/database/database";
-import { eq } from "drizzle-orm";
-import bcrypt from "bcrypt";
-
 export default defineEventHandler(async (event) => {
   const body = await readBody(event);
 
-  const db = await getDatabase();
-
-  const user = (await db.select().from(users).where(eq(users.email, body.email)).limit(1))?.[0];
-
-  // compare the password hash
-  if (!user || !bcrypt.compareSync(body.password, user.password)) {
-    // return an error if the user is not found or the password doesn't match
-    throw createError({
-      statusCode: 401,
-      statusMessage: "Invalid email or password",
-    });
-  }
-
-  // set the session
-  await setUserSession(event, {
-    user: {
-      id: user.id,
-      name: user.name,
-    },
-    loggedInAt: new Date(),
-  });
+  auth.attempt(event, body.email, body.password);
 
   await sendRedirect(event, "/", 302);
 });
--- server/utils/auth.ts
+++ server/utils/auth.ts
@@ -3,6 +3,7 @@ import type { H3Event } from "h3";
 import users from "~/database/schema/users";
 import { eq } from "drizzle-orm";
 import getDatabase from "~/database/database";
+import bcrypt from "bcrypt";
 
 // Logs the user in as the given user model
 async function login(event: H3Event<Request>, user: UserType) {
@@ -22,6 +23,26 @@ async function user(event) {
   const result = (await db.select().from(users).where(eq(users.id, session.user.id)).limit(1))?.[0];
   delete result.password;
   return result;
+}
+
+async function attempt(event: H3Event<Request>, email: string, password: string) {
+  const db = await getDatabase();
+
+  const user = (await db.select().from(users).where(eq(users.email, email)).limit(1))?.[0];
+
+  // compare the password hash
+  if (!user || !bcrypt.compareSync(body.password, password)) {
+    // return an error if the user is not found or the password doesn't match
+    throw createError({
+      statusCode: 401,
+      statusMessage: "Invalid email or password",
+    });
+  }
+
+  // log in as the selected user
+  await login(event, user);
+
+  return true;
 }
 
 export default {

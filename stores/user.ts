c8921c1c6cb4e53d26ab960a39864e83344d6dc3
David Nahodyl <david@gearboxgo.com>
2024-06-25 10:32:06 -0400

    added comments

--- stores/user.ts
+++ stores/user.ts
@@ -1,4 +1,7 @@
 export default defineStore("user", () => {
+  // we could get the use name from the session, which is just storing the ID and the name, but in this example we'll
+  // get additional user data from an API endpoint to demonstrate that process and keep the session data small
+
   const { data: user, refresh } = useFetch("/api/auth/me");
 
   async function refreshUser() {
